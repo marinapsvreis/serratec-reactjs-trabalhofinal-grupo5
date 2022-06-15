5c7196d816d8b0c842b03237b764129765b10089
Marina Portugal <marinapsvreis@gmail.com>
2022-06-15 19:45:12 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Button } from "./components/Button/Button";
+
+export function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </>
+  );
+}
