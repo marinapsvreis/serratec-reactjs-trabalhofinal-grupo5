3dd849d317e61be9f8eec793c8f739319f65ec94
Kauã Cassiano <87822196+KhawanC@users.noreply.github.com>
2022-06-16 10:14:06 -0300

    Pagina Sobre quase perfeitamente implementado

---
+++ src/Components/Carrossel/style.jsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const CarrosselStyle = styled.div`
+  position: relative;
+  width: 80%;
+
+  .itemCarrosel {
+    background-color: transparent;
+    color: white;
+
+    display: flex;
+
+    height: 20em;
+    font-size: 2em;
+    font-weight: bold;
+    
+    align-items: center;
+    justify-content: space-around;
+
+    .boxPessoaDesc {
+      width: 60%;
+    }
+  }
+
+  .navegacaoCarrosel {
+    position: absolute;
+
+    width: 100%;
+    padding: 20px 0;
+    bottom: 0;
+    left: 0;
+
+    text-align: center;
+  }
+
+  .botaoCarrosel {
+    background-color: rgba(255, 255, 255, 0.1);
+
+    display: inline-block;
+
+    width: 10px;
+    height: 10px;
+    margin: 0 5px;
+    border-radius: 50%;
+  }
+
+  .botaoCarroselAtivo {
+    background-color: rgba(255, 255, 255, 0.7);
+
+    display: inline-block;
+
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    margin: 0 5px;
+  }
+`;
--- src/Routes/Root.js
+++ src/Routes/Root.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home } from '../Pages/Home'
+import { Sobre } from '../Pages/Sobre/Index'
 
 export const Root = () => {
   return (
     <BrowserRouter>
       <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/sobre" element={<Sobre />} />
       </Routes>
     </BrowserRouter>
   )
