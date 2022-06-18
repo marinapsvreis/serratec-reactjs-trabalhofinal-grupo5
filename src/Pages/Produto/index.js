bfd05138e38f02269d2b53e64bd194f9f385619d
pedroCurioni <pedrocurioni@gmail.com>
2022-06-18 11:23:56 -0300

    consertando import da pagina produto

--- src/Pages/Produto/index.js
+++ src/Pages/Produto/index.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { DataContext } from '../../Context/data'
 import { useParams } from "react-router-dom";
 import { api } from "../../Services/api";
 import { Container } from "../global-style";
