9a9f9dc0b111a4ee32935c92a47389f5b044a712
pedroCurioni <pedrocurioni@gmail.com>
2022-06-19 13:06:01 -0300

    ajuste casos onde o carrinho nao sera mostrado

--- src/Pages/Carrinho/index.js
+++ src/Pages/Carrinho/index.js
@@ -10,62 +10,87 @@ export const Carrinho = () => {
   const [itemPedido, setItemPedido] = useState([]);
   const [produtos, setProdutos] = useState([]);
   const [display, setDisplay] = useState(null);
+  const [listExists, setListExists] = useState(false);
+  const [nextRequest, setNextRequest] = useState(false);
   const { idUsuario, handleSetIdUsuario } = useContext(DataContext);
 
   useEffect(() => {
-    const getPedidosByClienteId = async () => {
-      await api
-        .get(`pedido/cliente/${idUsuario}`)
-        .then((res) => setPedidos(res.data));
-    };
-    getPedidosByClienteId();
+    if (idUsuario === 0) {
+      setDisplay(
+        <Container>
+          <Titulo>Por favor faça login para vizualizar seu Carrinho</Titulo>
+        </Container>
+      );
+    } else {
+      const getPedidosByClienteId = async () => {
+        await api
+          .get(`pedido/cliente/${idUsuario}`)
+          .then((res) => setPedidos(res.data));
+      };
+      getPedidosByClienteId();
+      setNextRequest(true);
+    }
   }, [idUsuario]);
 
   useEffect(() => {
-    if (pedidos.length > 0) {
-      const getItemPedidoByPedidoId = async () => {
-        await api
-          .get(`itemPedido/pedido/${pedidos[pedidos.length - 1].idPedido}`)
-          .then((res) => setItemPedido(res.data));
-      };
-      getItemPedidoByPedidoId();
+    if (pedidos.length !== 0 && nextRequest === true) {
+      if (pedidos[pedidos.length - 1].status === true) {
+        console.log("teste 1");
+        setDisplay(<Titulo>Não há items em seu carrinho</Titulo>);
+      } else {
+        setNextRequest(false);
+        const getItemPedidoByPedidoId = async () => {
+          await api
+            .get(`itemPedido/pedido/${pedidos[pedidos.length - 1].idPedido}`)
+            .then((res) => setItemPedido(res.data));
+        };
+        getItemPedidoByPedidoId();
+        setNextRequest(true);
+      }
     }
   }, [pedidos]);
 
   useEffect(() => {
-    if (itemPedido.length > 0) {
-      let newProduto = [];
-      itemPedido.forEach((response) => {
-        const getProduto = async () => {
-          await api
-            .get(`produto/${response.idProduto}`)
-            .then((res) => newProduto.push(res.data));
-        };
-        getProduto();
-      });
-      setProdutos(newProduto);
-      if(newProduto.length >0){
-        
+    if (nextRequest === true) {
+      if (itemPedido.length === 0 && display === null) {
+        console.log(itemPedido);
+        setDisplay(<Titulo>Não ha items em seu carrinho</Titulo>);
+      } else {
+        let newProduto = [];
+        itemPedido.forEach((response) => {
+          const getProduto = async () => {
+            await api
+              .get(`produto/${response.idProduto}`)
+              .then((res) => newProduto.push(res.data));
+          };
+          getProduto();
+        });
+        setProdutos(newProduto);
+        setListExists(true);
       }
     }
   }, [itemPedido]);
-  
-  function handleDisplay(){
-    setDisplay(
-      produtos.map((res) => {
-        const itemPedidoFiltrado =  itemPedido.filter((response) => response.idProduto == res.idProduto)
-        return (
-          <ItemCarrinho
-            produto={res}
-            itemPedido={itemPedidoFiltrado}
-            key={res.idProduto}
-          />
-        );
-      })
-    );
+
+  function handleDisplay() {
+    if (listExists) {
+      setDisplay(
+        produtos.map((res) => {
+          const itemPedidoFiltrado = itemPedido.filter(
+            (response) => response.idProduto == res.idProduto
+          );
+          return (
+            <ItemCarrinho
+              produto={res}
+              itemPedido={itemPedidoFiltrado}
+              key={res.idProduto}
+            />
+          );
+        })
+      );
+    }
   }
 
-  const load = setTimeout(handleDisplay,500)
+  const load = setTimeout(handleDisplay, 300);
 
   return (
     <Container>
