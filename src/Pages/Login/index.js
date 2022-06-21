80f88a77dde2a60fa73b6c4df68ca703fb78c985
Kauã Cassiano <87822196+KhawanC@users.noreply.github.com>
2022-06-21 17:16:52 -0300

    feedback autenticação de cliente 100%

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,19 +1,23 @@
 import React, {useState, useEffect, useContext} from "react";
 import {Link, useNavigate} from "react-router-dom"
 import { Container, Titulo } from "../global-style";
-import { AdmButton, Form, FormInput, LoginButton, RegistroButton } from "./style";
+import { AdmButton, Form, FormInput, LoginButton, PopupStyle, RegistroButton } from "./style";
 import {api} from '../../Services/api'
 import { DataContext } from "../../Context/data";
+import { Loader } from "../../Components/Loader";
 
 export const Login = () => {
   const { idUsuario, handleSetIdUsuario } = useContext(DataContext);
-
+  const [statusCliente, setStatusCliente] = useState(0)
+  const [statusCliente1, setStatusCliente1] = useState(0)
+  const [isLoading, setLoading] = useState(false)
+  const [isEmailCerto, setEmailCerto] = useState(false)
   const [clienteList, setClienteList] = useState([]);
-  let cliente;
-  let idCliente;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   let navigate = useNavigate();
+  let cliente;
+  let idCliente;
 
 
   useEffect(()=>{
@@ -30,9 +34,10 @@ export const Login = () => {
   function getIdCliente (email){
     cliente = clienteList.filter(c => c.email === email)
     if(cliente[0] === undefined){
-      alert('Email não cadastrado. Cadastre-se!')
-      navigate("../registro")
+      setStatusCliente1(e => 401)
+      setEmailCerto(false)
     }else{
+      setEmailCerto(true)
       idCliente = cliente[0].idCliente;
       localStorage.setItem('idCliente', cliente[0].idCliente)
       localStorage.setItem('nomeCliente', cliente[0].nomeCompleto)      
@@ -49,16 +54,13 @@ export const Login = () => {
 
   const allowLogin = async (email, password) => {
     try{
-      const response = await api.get(`cliente/logar_cliente?email=${email}&password=${password}`)
-        alert('Sucesso!');
-        handleSetIdUsuario(idCliente);
-        navigate("../catalogo");
-        window.location.reload(true);
-    }catch(e){
-      alert('Senha incorreta!')
+      const res = await api.get(`cliente/logar_cliente?email=${email}&password=${password}`)
+      setStatusCliente(e => res.status)
+      handleSetIdUsuario(idCliente);
+    }catch(error){
+      setStatusCliente(e => 400)
       localStorage.removeItem('idCliente')
       localStorage.removeItem('nomeCliente')
-      window.location.reload(true);
     }          
   }
 
@@ -71,17 +73,68 @@ export const Login = () => {
     navigate("../admlogin")
   }
 
+  function load() {
+    setLoading(e => true)
+    setTimeout(function() {
+      setLoading(e => false)
+      logar()
+    }, 800)
+  }
+
   return (
-    <Container>
+    <>
+      <Container>
       <Titulo>Login Cliente</Titulo>
       <Form>
         <FormInput onChange={e => handleEmailChange(e)} type="text" placeholder="Email"/>
         <FormInput onChange={e => handlePasswordChange(e)} type="password" placeholder="Senha"/>
-        <LoginButton onClick={logar}>Entrar</LoginButton>
+        <LoginButton onClick={load}>Entrar</LoginButton>
         <Link to="/registro" style={{textDecoration: "none"}}><RegistroButton>Não tem uma conta? Cadastre-se</RegistroButton></Link>                
       </Form>
 
       <AdmButton onClick={gotToAdm}>Login Admin</AdmButton>
     </Container>
+    {isLoading === true ? <>
+      <PopupStyle>
+                <div className='popup-tela'>
+                  <p>Carregando...</p>
+                  <Loader/>
+                </div>
+            </PopupStyle> 
+    </> : ''}
+    {isLoading === false && statusCliente === 200 ? <>
+      <PopupStyle>
+          <div className='popup-tela'>
+            <p>Cliente autenticado com sucesso!</p>
+            <div className="botoes">
+              <button onClick={() => {navigate('../catalogo'); window.location.reload(true)}}>OK</button>
+            </div>
+          </div>
+        </PopupStyle> 
+    </> : ''}
+    {isLoading === false && statusCliente === 400 && isEmailCerto === true ? <>
+      <PopupStyle>
+          <div className='popup-tela'>
+            <h2>Sua senha está errada...</h2>
+            <p>Tente novamente</p>
+            <div className="botoes">
+              <button onClick={() => setStatusCliente(e => 0)}>OK</button>
+            </div>
+          </div>
+        </PopupStyle>
+    </> : ''}
+    {isLoading === false && statusCliente1 === 401 && isEmailCerto === false ? <>
+      <PopupStyle>
+          <div className='popup-tela'>
+            <h2>Parece que você não tem um email!</h2>
+            <p>Por favor, crie um email</p>
+            <div className="botoes">
+              <button onClick={() => {navigate("../registro"); window.location.reload(true)}}>OK</button>
+            </div>
+          </div>
+        </PopupStyle>
+    </> : ''}
+    
+    </>
   );
 };
--- src/Pages/Login/style.js
+++ src/Pages/Login/style.js
@@ -51,3 +51,69 @@ export const AdmButton = styled.button`
     background-color: var(--secondary-color);
   }
 `;
+
+export const PopupStyle = styled.div`
+background-color: rgba(0, 0, 0, 0.5);
+
+display: flex;
+
+position: fixed;
+top: 0;
+left: 0;
+width: 100%;
+height: 100vh;
+
+justify-content: center;
+align-items: center;
+
+z-index: 5;
+
+.popup-tela {
+    background-color: gray;
+
+    display: flex;
+    flex-direction: column;
+
+    padding: 40px;
+    width: 55em;
+
+    align-items: center;
+    justify-content: center;
+
+    text-align: center;
+
+    border: 2px solid black;
+    border-radius: 10px;
+
+    z-index: 6;
+    
+}
+
+.popup-tela p {
+    font-size: 1.7em;
+}
+
+.popup-tela .botoes {
+    display: flex;
+    gap: 20%;
+}
+
+.popup-tela .botoes button {
+    background-color: var(--secondary-color);
+
+    font-size: 1.3em;
+
+    width: 7em;
+
+    padding: 3px;
+
+    border-radius: 10px;
+
+    transition: 250ms;
+}
+
+.popup-tela .botoes button:hover {
+    background-color: var(--primary-color);
+}
+
+`;
