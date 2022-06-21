aeddb81649d459650f92737ceb323a0753b67e5c
Kauã Cassiano <87822196+KhawanC@users.noreply.github.com>
2022-06-21 14:10:11 -0300

    ajuste z-index e alguns botoes

--- src/Components/EditarProduto/style.js
+++ src/Components/EditarProduto/style.js
@@ -14,6 +14,8 @@ height: 100vh;
 justify-content: center;
 align-items: center;
 
+z-index: 5;
+
 .popup-tela {
     background-color: gray;
 
@@ -30,6 +32,8 @@ align-items: center;
 
     border: 2px solid black;
     border-radius: 10px;
+
+    z-index: 6;
     
 }
 
